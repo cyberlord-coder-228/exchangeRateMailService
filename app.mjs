b4fc6eb21ca7578beb74ae2195322141636623f3
vlad <cyberlord2003@gmail.com>
2024-08-10 23:34:35 +0300

    database initialization tweaked

--- app.mjs
+++ app.mjs
@@ -5,6 +5,7 @@ dotenv.config();
 
 import express from 'express';
 import cron from 'node-cron';
+import Database from 'better-sqlite3';
 
 import { router } from './src/routes/exchangeRate.routes.mjs';
 import { notifyAllUsers } from './src/services/broadcast.services.mjs';
@@ -14,14 +15,27 @@ const app = express();
 
 app.use(express.json());
 
+try {
+  const db = Database('users.db');
+  const createDatabase = `
+    CREATE TABLE emails (
+      id INTEGER PRIMARY KEY,
+      email STRING NOT NULL UNIQUE
+    )
+  `;
+  db.exec(createDatabase);
+} catch(e) {
+  console.log(e);
+}
+
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.use('/', router);
 
 app.listen(port, () => {
-	console.log(`Working on port ${port}`);
+  console.log(`Working on port ${port}`);
 });
 
 cron.schedule('0 12 * * 1', notifyAllUsers);
